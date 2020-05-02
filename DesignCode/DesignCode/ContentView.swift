879c585bf71af89a6f2016ee16a716e36850f41a
Wayne Dahlberg <waynefj40@gmail.com>
2020-05-01 20:29:11 -0600

    Initial commit
    Added Xcode project

---
+++ DesignCode/DesignCode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DesignCode
+//
+//  Created by Wayne Dahlberg on 5/1/20.
+//  Copyright © 2020 Wayne Dahlberg. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
